812cc9ea9adaf263696f55466495817aba0ff096
namtao97 <nguyentiennam97@gmail.com>
2018-10-14 00:24:20 +0700

    BallTracking & FaceDetection

---
+++ FaceDetection/facial_landmarks.py
@@ -0,0 +1,15 @@
+# import the necessary packages
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import dlib
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="path to facial landmark predictor")
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+args = vars(ap.parse_args())
